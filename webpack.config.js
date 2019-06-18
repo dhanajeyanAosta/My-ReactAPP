4652f37ccd37a4c588a9402ed40f33a47c7350d1
dhanajeyanAosta <dhanajeyan@gmail.com>
2019-06-18 22:28:29 +0530

    server.js file created

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: './main.js',
+   entry: './index.js',
    output: {
-      path: path.join(__dirname, '/bundle'),
-      filename: 'index_bundle.js'
+      path: path.join(__dirname, ''),
+      filename: 'bundle.js'
       //filename: 'index.js'
    },
    devServer: {
